d7123d2e0b0d6b9fe5a565ddfee5afb3ca95ce70
Samuel Peters <sampeters@csumb.edu>
2019-04-21 19:20:23 +0000

    login connected to search page

--- right_review_codebase/index.php
+++ right_review_codebase/index.php
@@ -3,7 +3,7 @@
 
     <head>
         <meta charset="utf-8" />
-        <title>Samuel Peters: Personal Website</title>
+        <title>Logout Example</title>
     </head>
     
     <body>
@@ -11,10 +11,22 @@
         
         <nav></nav>
         
-        <main></main>
+        <main>
+            <button id="logoutButton">Logout</button>
+        </main>
         
         <footer></footer>
 
     </body>
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <script>
+        $("#logoutButton").on("click", function() {
+            window.location = "api/logout.php";
+        })
+    </script>
 
 </html>
