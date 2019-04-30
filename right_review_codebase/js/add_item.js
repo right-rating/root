752283e7db87df0b532e3f16083350fe3193ff8c
Alexandria Davis <alexandriadavis@csumb.edu>
2019-04-29 17:50:12 -0700

    Fix submission of new website on page load

--- right_review_codebase/js/add_item.js
+++ right_review_codebase/js/add_item.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-  console.log("And we have scripts");
   function submit_page()
   {
             $.ajax({
@@ -14,27 +13,13 @@ $(document).ready(function() {
 
                 },
                 success: function(data,status){
-
+                  //WindowClient.navigate("/")
                 },
                 complete: function(data, status){
 
                 }
             })
 
-    /*
-    <form class="" action="index.html" method="post">
-      <label for="item_name">Website Name</label>
-      <input type="text" name="item_name" value=""></input>
-      <label for="Description"> Description </label>
-      <input type="text" name="Description" value=""></input>
-      <label for="item_image">Image URL:</label>
-      <input type="text" name="item_image" value=""></input>
-      <button type="button" name="submit" id="submit_webpage"></button>
-      */
-
-
-    console.log("submitting")
   };
   $("#submit_webpage").click(submit_page);
-  submit_page();
 });
