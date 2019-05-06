42a70fbce6438feed3c554ba3b63c2b9837dc3fc
Juhi Park <juhpark@csumb.edu>
2019-05-06 03:54:23 +0000

    advanced search feature

--- right_review_codebase/api/search.php
+++ right_review_codebase/api/search.php
@@ -3,8 +3,6 @@
     
     $conn = getDBConn();
 
-    // $sql = "SELECT * FROM item ORDER BY item_id ";
-    
     // $sql = "SELECT item_id, item_name, item_description FROM rating INNER JOIN item ON item.item_id = rating.item_id";
     // $sql = "SELECT item.item_id, AVG(rating.rating_val) FROM item INNER JOIN rating ON rating.item_id = item.item_id GROUP BY item.item_id";
     
