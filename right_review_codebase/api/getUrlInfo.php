0e2ce2cb67a15d995aa2bcc6d8c36e6c43a5ca83
Juhi Park <juhpark@csumb.edu>
2019-05-06 19:56:19 +0000

    Google safe browsing api

---
+++ right_review_codebase/api/getUrlInfo.php
@@ -0,0 +1,18 @@
+<?php
+
+//connect to database
+include 'DBConnection.php';
+$conn = getDBConn();
+
+$np = array();
+$np[':item_id'] = $_GET['item_id'];
+
+$sql = 'SELECT item_name 
+        FROM item 
+        WHERE item_id = :item_id;';
+
+$stmt = $conn->prepare($sql);
+$stmt->execute($np);
+$records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+echo json_encode($records);
+?>
