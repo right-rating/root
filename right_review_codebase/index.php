3af0a9e5d05174cdd44e8bd1809bcc9ac70190f9
Samuel Peters <sampeters@csumb.edu>
2019-05-08 23:38:50 +0000

    search without login

--- right_review_codebase/index.php
+++ right_review_codebase/index.php
@@ -31,6 +31,9 @@
           <li class="nav-item">
             <a class="nav-link" href="signup.html">Signup</a>
           </li>
+          <li class="nav-item">
+            <a class="nav-link" href="search.php">Search</a>
+          </li>
         </ul>
       </div>
     </nav>
