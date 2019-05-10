a9f8fce4bfc4790feca0aefd1ace819bea976c83
Samuel Peters <sampeters@csumb.edu>
2019-05-10 17:16:12 +0000

    search page goes displays all websites now

--- right_review_codebase/api/search.php
+++ right_review_codebase/api/search.php
@@ -6,7 +6,7 @@
     // $sql = "SELECT item_id, item_name, item_description FROM rating INNER JOIN item ON item.item_id = rating.item_id";
     // $sql = "SELECT item.item_id, AVG(rating.rating_val) FROM item INNER JOIN rating ON rating.item_id = item.item_id GROUP BY item.item_id";
     
-    $sql = "SELECT item.item_id, item.item_name, item.item_description, AVG(rating.rating_val) FROM item INNER JOIN rating ON rating.item_id = item.item_id GROUP BY item.item_id";
+    $sql = "SELECT item.item_id, item.item_name, item.item_description, AVG(rating.rating_val) FROM item LEFT JOIN rating ON rating.item_id = item.item_id GROUP BY item.item_id";
 
     $stmt = $conn->prepare($sql);
     $stmt->execute();
